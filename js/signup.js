26a53b5e60c8000e61b993a18d7c1d22ce5edbd2
Aditya Kiran Pentyala <Aditya Kiran Pentyala>
2017-04-08 17:39:09 -0700

    Added a new file

--- js/signup.js
+++ js/signup.js
@@ -11,8 +11,8 @@ $(function() {
             $('#register-form-link').click(function(e) {
                 $("#register-form").delay(100).fadeIn(100);
                 $("#login-form").fadeOut(100);
-                $("#login-form-link").style.color = "#666";
-                $('#register-form-link').style.color = "#2d3b55";
+                $("#login-form-link").css("color", "#666");
+                $('#register-form-link').css("color", "#2d3b55");
                 $('#login-form-link').removeClass('active');
                 $(this).addClass('active');
                 e.preventDefault();
